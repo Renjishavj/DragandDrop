12545bee01d1b04af155efc7a6cd6f8de2bdd1ba
Renjishavj <renjishavj@gmail.com>
2024-01-08 11:51:29 +0530

    DragDrop

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+import { Dropzone, FileMosaic } from "@files-ui/react";
+
+function App() {
+  const [files, setFiles] = React.useState([]);
+  const updateFiles = (incommingFiles) => {
+    setFiles(incommingFiles);
+  };
+  return (
+    <Dropzone onChange={updateFiles} value={files}>
+      {files.map((file) => (
+        <FileMosaic {...file} preview />
+      ))}
+    </Dropzone>
+  );
+}
+
+export default App
